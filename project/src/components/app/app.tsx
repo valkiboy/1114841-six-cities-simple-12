76c80fdcb4435818403b67f850e6951997d3ae49
unknown <val_kik@mail.ru>
2023-03-04 11:14:23 +0300

    Создает страницы, компонент карточки и пропсы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,17 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+// import MainPage from '../../pages/main/main-page';
+// import RoomPage from '../../pages/room/room-page';
+import LoginPage from '../../pages/login/login-page';
+
+type AppPageProps = {
+  placesCount: number;
+}
+
+function App({placesCount}:AppPageProps): JSX.Element {
+  return (
+    // <MainPage placesCount={placesCount}/>
+    // <RoomPage />
+    <LoginPage />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  PlacesCount: 312,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App placesCount = {Setting.PlacesCount} />
   </React.StrictMode>,
 );
